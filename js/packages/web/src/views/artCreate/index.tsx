5508bb1a8c6d60de4e47221b57c3abe3e6ebd57b
cruzor <gurungabish16@gmail.com>
2022-01-05 16:36:21 +0545

    feat(Category): change category step ui

--- js/packages/web/src/views/artCreate/index.tsx
+++ js/packages/web/src/views/artCreate/index.tsx
@@ -241,74 +241,72 @@ const CategoryStep = (props: {
       <Row className="call-to-action">
         <h2>Create a new item</h2>
         <p>
-          First time creating on Metaplex?{' '}
-          <a href="https://docs.metaplex.com/create-store/sell" target="_blank" rel="noreferrer">
+          First time creating on Run The Chain?{' '}
+          {/*<a href="https://docs.metaplex.com/create-store/sell" target="_blank" rel="noreferrer">
             Read our creators’ guide.
-          </a>
+          </a>*/}
         </p>
       </Row>
-      <Row justify={width < 768 ? 'center' : 'start'}>
-        <Col>
-          <Row>
-            <Button
-              className="type-btn"
-              size="large"
-              onClick={() => props.confirm(MetadataCategory.Image)}
-            >
-              <div>
-                <div>Image</div>
-                <div className="type-btn-description">JPG, PNG, GIF</div>
-              </div>
-            </Button>
-          </Row>
-          <Row>
-            <Button
-              className="type-btn"
-              size="large"
-              onClick={() => props.confirm(MetadataCategory.Video)}
-            >
-              <div>
-                <div>Video</div>
-                <div className="type-btn-description">MP4, MOV</div>
-              </div>
-            </Button>
-          </Row>
-          <Row>
-            <Button
-              className="type-btn"
-              size="large"
-              onClick={() => props.confirm(MetadataCategory.Audio)}
-            >
-              <div>
-                <div>Audio</div>
-                <div className="type-btn-description">MP3, WAV, FLAC</div>
-              </div>
-            </Button>
-          </Row>
-          <Row>
-            <Button
-              className="type-btn"
-              size="large"
-              onClick={() => props.confirm(MetadataCategory.VR)}
-            >
-              <div>
-                <div>AR/3D</div>
-                <div className="type-btn-description">GLB</div>
-              </div>
-            </Button>
-          </Row>
-          <Row>
-            <Button
-              className="type-btn"
-              size="large"
-              onClick={() => props.confirm(MetadataCategory.HTML)}
-            >
-              <div>
-                <div>HTML Asset</div>
-                <div className="type-btn-description">HTML</div>
-              </div>
-            </Button>
-          </Row>
+      <Row gutter={40}>
+        <Col className="gutter-row" span={6}>
+          <Button
+            className="type-btn"
+            size="large"
+            onClick={() => props.confirm(MetadataCategory.Image)}
+          >
+            <div>
+              <div>Image</div>
+              <div className="type-btn-description">JPG, PNG, GIF</div>
+            </div>
+          </Button>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <Button
+            className="type-btn"
+            size="large"
+            onClick={() => props.confirm(MetadataCategory.Video)}
+          >
+            <div>
+              <div>Video</div>
+              <div className="type-btn-description">MP4, MOV</div>
+            </div>
+          </Button>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <Button
+            className="type-btn"
+            size="large"
+            onClick={() => props.confirm(MetadataCategory.Audio)}
+          >
+            <div>
+              <div>Audio</div>
+              <div className="type-btn-description">MP3, WAV, FLAC</div>
+            </div>
+          </Button>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <Button
+            className="type-btn"
+            size="large"
+            onClick={() => props.confirm(MetadataCategory.VR)}
+          >
+            <div>
+              <div>AR/3D</div>
+              <div className="type-btn-description">GLB</div>
+            </div>
+          </Button>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <Button
+            className="type-btn"
+            size="large"
+            onClick={() => props.confirm(MetadataCategory.HTML)}
+          >
+            <div>
+              <div>HTML Asset</div>
+              <div className="type-btn-description">HTML</div>
+            </div>
+          </Button>
         </Col>
       </Row>
     </>
